b7eeca069ba4af25dee422dd44159ee6e171f7e4
IsaacSemb <isaacsemb1996@gmail.com>
2025-03-19 17:19:38 +0000

    created url in the local (books app) and then added the whole url configuration in the main settings urls

--- djg_project_1/book_keeper/views.py
+++ djg_project_1/book_keeper/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def intro ():
+def intro (request):
     return HttpResponse('<h1>welcome to the book keeper</h1>')
