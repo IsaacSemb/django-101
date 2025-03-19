7624fb4a23fce5239607055ae091c5ac9e2f9c64
IsaacSemb <isaacsemb1996@gmail.com>
2025-03-19 18:29:33 +0000

    created templates and learned to return different types of templates

--- djg_project_1/book_keeper/views.py
+++ djg_project_1/book_keeper/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+# return some html from a render 
 def intro (request):
     return HttpResponse('<h1>welcome to the book keeper</h1>')
+
+# returning an entire template
+def welcome(request):
+    return (render(request, 'welcome.html'))
+
+# returning an template with some logic
+def welcome(request):
+    name = 'Isaac Ssembuusi'
+    return (render(request, 'welcome.html',{'names':name}))
