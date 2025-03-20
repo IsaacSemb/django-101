5a9887dc0b8e29eaa829e73078394f1b5559f464
IsaacSemb <isaacsemb1996@gmail.com>
2025-03-20 04:03:14 +0000

    created a migration with makemigrations command
    this is whenever you touch the models file, make migrations
    
    django translates the ORM stuff into sql code

--- djg_project_1/book_keeper/models.py
+++ djg_project_1/book_keeper/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    ISBN = models.CharField(max_length=15)
+    title = models.CharField(max_length=255)
+    year_of_publication = models.IntegerField()
+    publisher = models.CharField(max_length=255)
+    image_url_small = models.URLField(max_length=200)
+    image_url_medium = models.URLField(max_length=200) 
+    image_url_large = models.URLField(max_length=200)
