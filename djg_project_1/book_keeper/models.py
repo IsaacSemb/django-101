cd07406e279e0acef2e59e92321c2f393e047e31
IsaacSemb <isaacsemb1996@gmail.com>
2025-03-20 04:26:30 +0000

    learning migration undoing - created a migration that will be undone

--- djg_project_1/book_keeper/models.py
+++ djg_project_1/book_keeper/models.py
@@ -5,8 +5,15 @@ from django.db import models
 class Book(models.Model):
     ISBN = models.CharField(max_length=15)
     title = models.CharField(max_length=255)
+    slug = models.SlugField(null=True)
     year_of_publication = models.IntegerField()
     publisher = models.CharField(max_length=255)
     image_url_sm = models.URLField(max_length=200)
     image_url_md = models.URLField(max_length=200) 
     image_url_lg = models.URLField(max_length=200)
+    
+    class Meta:
+        db_table = 'books'
+        indexes = [
+            models.Index( fields=['ISBN'] )
+            ]
