574f875d0be28153620360977c4f57475e286abf
IsaacSemb <isaacsemb1996@gmail.com>
2025-03-20 06:07:24 +0000

    correcting data issues to fit the database hence created so many migrations

--- djg_project_1/book_keeper/models.py
+++ djg_project_1/book_keeper/models.py
@@ -3,14 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
+    id = models.IntegerField(primary_key=True)
     ISBN = models.CharField(max_length=15)
     title = models.CharField(max_length=255)
-    slug = models.SlugField(null=True)
-    year_of_publication = models.IntegerField()
+    book_author = models.CharField(max_length=255)
+    year_of_publication = models.CharField(max_length=255)
     publisher = models.CharField(max_length=255)
     image_url_sm = models.URLField(max_length=200)
     image_url_md = models.URLField(max_length=200) 
     image_url_lg = models.URLField(max_length=200)
+    # slug = models.SlugField(null=True)
     
     # class Meta:
     #     db_table = 'books'
